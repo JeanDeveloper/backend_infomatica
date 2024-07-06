928dacf0c35cfd0f12fb7d134168c3905e36e1f3
JeanDeveloper <jecachusa.1996@gmail.com>
2024-07-06 11:56:44 -0500

    my first commit

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,73 @@
+﻿using DB;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace infomatica.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        private readonly InfomaticaContext _context;
+        private readonly IConfiguration _configuration;
+
+        public AuthController(InfomaticaContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] UserLogin login)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Username == login.Username && u.Password == login.Password);
+
+            if (user == null)
+            {
+                return Unauthorized("Invalid credentials");
+            }
+
+            //var token = GenerateJwtToken(user);
+            return Ok(user);
+        }
+
+
+        [HttpPost("logout")]
+        public IActionResult Logout()
+        {
+            // Logout logic can be handled on the client-side by deleting the token.
+            return Ok("Logged out");
+        }
+
+        //private string GenerateJwtToken(User user)
+        //{
+        //    var claims = new[]
+        //    {
+        //        new Claim(JwtRegisteredClaimNames.Sub, user.Username),
+        //        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        //    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+        //    var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+        //    var token = new JwtSecurityToken(
+        //        issuer: _configuration["Jwt:Issuer"],
+        //        audience: _configuration["Jwt:Audience"],
+        //        claims: claims,
+        //        expires: DateTime.Now.AddMinutes(30),
+        //        signingCredentials: creds);
+        //    return new JwtSecurityTokenHandler().WriteToken(token);
+        //}
+
+    }
+
+    public class UserLogin
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Controllers/SubCategoryController.cs
@@ -0,0 +1,104 @@
+﻿using DB;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace infomatica.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubCategoryController : ControllerBase
+    {
+
+        private InfomaticaContext _context;
+
+        public SubCategoryController(InfomaticaContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET: api/SubCategory
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<SubCategory>>> GetSubCategories()
+        {
+            return await _context.SubCategories.Include( sc => sc.Category ).ToListAsync();
+        }
+
+
+        // GET: api/SubCategory/2
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SubCategory>> GetSubCategory(int id)
+        {
+            var Subcategory = await _context.SubCategories.FindAsync(id);
+
+            if (Subcategory == null)
+            {
+                return NotFound();
+            }
+
+            return Subcategory;
+        }
+
+        // POST: api/SubCategory
+        [HttpPost]
+        public async Task<ActionResult<SubCategory>> PostSubCategory(SubCategory subcategory)
+        {
+            _context.SubCategories.Add(subcategory);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSubCategories", new { id = subcategory.Id }, subcategory);
+        }
+
+        // PUT: api/SubCategory/2
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSubCategory(int id, SubCategory subcategory)
+        {
+            if (id != subcategory.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(subcategory).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SubCategoryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/SubCategory/2
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSubCategory(int id)
+        {
+            var subcategory = await _context.SubCategories.FindAsync(id);
+            if (subcategory == null)
+            {
+                return NotFound();
+            }
+
+            _context.SubCategories.Remove(subcategory);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool SubCategoryExists(int id)
+        {
+            return _context.SubCategories.Any(e => e.Id == id);
+        }
+
+    }
+}
